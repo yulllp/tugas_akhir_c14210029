12ba25b71c4d933d5b5d01af9fd2a1e024e96409
yulllllll <ceoofyulll@gmail.com>
2025-06-24 05:10:26 +0700

    fix 1

--- resources/views/livewire/customer/customer-table.blade.php
+++ resources/views/livewire/customer/customer-table.blade.php
@@ -80,13 +80,13 @@
                                         class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                         Edit
                                     </a>
-                                    <form action="{{ route('customers.destroy', $customer->id) }}" method="POST" onsubmit="return confirm('Apakah anda yakin akan menghapus pelanggan ini?');">
+                                    <!-- <form action="{{ route('customers.destroy', $customer->id) }}" method="POST" onsubmit="return confirm('Apakah anda yakin akan menghapus pelanggan ini?');">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                                             Hapus
                                         </button>
-                                    </form>
+                                    </form> -->
                                 </td>
                             </tr>
                             @endforeach
